cf6ea9fe8afe79d887d618ac2d4d9aecfc5bd980
Matrix <matrixofkings@gmail.com>
2019-02-13 17:09:49 +0200

    Initial commit (Update android studio)

---
+++ app/src/main/java/com/almoalm/kinan/coderswag/Controller/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.almoalm.kinan.coderswag.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.almoalm.kinan.coderswag.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
